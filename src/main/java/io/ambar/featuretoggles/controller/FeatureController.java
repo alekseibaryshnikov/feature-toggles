f70f10902ceeb1338005d93e7f46f7ce89a9a256
Aleksei Baryshnikov <a250188@yandex.ru>
2020-08-28 01:23:11 +0300

    Removed useless imports

--- src/main/java/io/ambar/featuretoggles/controller/FeatureController.java
+++ src/main/java/io/ambar/featuretoggles/controller/FeatureController.java
@@ -1,10 +1,7 @@
 package io.ambar.featuretoggles.controller;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
